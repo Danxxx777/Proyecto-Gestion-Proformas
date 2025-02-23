11453e99e3fc559243bea007cb965ce4a276c5e1
Danxxx <loorbyron714@gmail.com>
2025-02-23 01:20:28 -0500

    Formulario Proveedores

--- Proformas/Formularios/frmProveedores.Designer.cs
+++ Proformas/Formularios/frmProveedores.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Proformas.Formularios
 {
-    partial class frmTiposPS
+    partial class frmProveedores
     {
         /// <summary>
         /// Required designer variable.
@@ -30,7 +30,7 @@
         {
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTiposPS));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmProveedores));
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
@@ -43,8 +43,6 @@
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges13 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges14 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges15 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges16 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             guna2CustomGradientPanel1 = new Guna.UI2.WinForms.Guna2CustomGradientPanel();
             lblClientes = new Label();
             pbxCerrar = new PictureBox();
@@ -57,8 +55,7 @@
             Editar = new Guna.UI2.WinForms.Guna2GradientButton();
             btnEliminar = new Guna.UI2.WinForms.Guna2GradientButton();
             btnAgregar = new Guna.UI2.WinForms.Guna2GradientButton();
-            dataGridView2 = new DataGridView();
-            guna2TextBox1 = new Guna.UI2.WinForms.Guna2TextBox();
+            pictureBox4 = new PictureBox();
             guna2GradientButton1 = new Guna.UI2.WinForms.Guna2GradientButton();
             guna2CustomGradientPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pbxCerrar).BeginInit();
@@ -66,7 +63,7 @@
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
             SuspendLayout();
             // 
             // guna2CustomGradientPanel1
@@ -78,27 +75,27 @@
             guna2CustomGradientPanel1.FillColor2 = Color.FromArgb(64, 0, 64);
             guna2CustomGradientPanel1.FillColor3 = Color.DarkViolet;
             guna2CustomGradientPanel1.FillColor4 = Color.DeepSkyBlue;
-            guna2CustomGradientPanel1.Location = new Point(-1, -2);
+            guna2CustomGradientPanel1.Location = new Point(-1, 0);
             guna2CustomGradientPanel1.Name = "guna2CustomGradientPanel1";
             guna2CustomGradientPanel1.ShadowDecoration.CustomizableEdges = customizableEdges2;
-            guna2CustomGradientPanel1.Size = new Size(859, 53);
-            guna2CustomGradientPanel1.TabIndex = 30;
+            guna2CustomGradientPanel1.Size = new Size(635, 53);
+            guna2CustomGradientPanel1.TabIndex = 44;
             // 
             // lblClientes
             // 
             lblClientes.AutoSize = true;
             lblClientes.Font = new Font("Mongolian Baiti", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lblClientes.Location = new Point(378, 11);
+            lblClientes.Location = new Point(248, 9);
             lblClientes.Name = "lblClientes";
-            lblClientes.Size = new Size(71, 25);
+            lblClientes.Size = new Size(142, 25);
             lblClientes.TabIndex = 1;
-            lblClientes.Text = "Tipos";
+            lblClientes.Text = "Proveedores";
             // 
             // pbxCerrar
             // 
             pbxCerrar.BackColor = Color.Transparent;
             pbxCerrar.Image = (Image)resources.GetObject("pbxCerrar.Image");
-            pbxCerrar.Location = new Point(798, 11);
+            pbxCerrar.Location = new Point(545, 3);
             pbxCerrar.Name = "pbxCerrar";
             pbxCerrar.Size = new Size(40, 36);
             pbxCerrar.SizeMode = PictureBoxSizeMode.StretchImage;
@@ -108,40 +105,40 @@
             // pictureBox3
             // 
             pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
-            pictureBox3.Location = new Point(391, 68);
+            pictureBox3.Location = new Point(47, 162);
             pictureBox3.Name = "pictureBox3";
             pictureBox3.Size = new Size(40, 35);
             pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox3.TabIndex = 39;
+            pictureBox3.TabIndex = 53;
             pictureBox3.TabStop = false;
             // 
             // pictureBox2
             // 
             pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
-            pictureBox2.Location = new Point(488, 68);
+            pictureBox2.Location = new Point(47, 250);
             pictureBox2.Name = "pictureBox2";
             pictureBox2.Size = new Size(40, 35);
             pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox2.TabIndex = 38;
+            pictureBox2.TabIndex = 52;
             pictureBox2.TabStop = false;
             // 
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(296, 68);
+            pictureBox1.Location = new Point(47, 76);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(40, 35);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox1.TabIndex = 37;
+            pictureBox1.TabIndex = 51;
             pictureBox1.TabStop = false;
             // 
             // dataGridView1
             // 
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(32, 227);
+            dataGridView1.Location = new Point(150, 204);
             dataGridView1.Name = "dataGridView1";
-            dataGridView1.Size = new Size(301, 181);
-            dataGridView1.TabIndex = 36;
+            dataGridView1.Size = new Size(434, 181);
+            dataGridView1.TabIndex = 50;
             // 
             // gtbUsuario
             // 
@@ -152,7 +149,7 @@
             gtbUsuario.BorderStyle = System.Drawing.Drawing2D.DashStyle.Dot;
             gtbUsuario.BorderThickness = 0;
             gtbUsuario.CustomizableEdges = customizableEdges3;
-            gtbUsuario.DefaultText = "Buscar Servicio";
+            gtbUsuario.DefaultText = "Buscar Proveedor";
             gtbUsuario.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
             gtbUsuario.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
             gtbUsuario.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
@@ -160,13 +157,13 @@
             gtbUsuario.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
             gtbUsuario.Font = new Font("Segoe UI", 9F);
             gtbUsuario.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
-            gtbUsuario.Location = new Point(129, 180);
+            gtbUsuario.Location = new Point(247, 151);
             gtbUsuario.Name = "gtbUsuario";
             gtbUsuario.PlaceholderText = "";
             gtbUsuario.SelectedText = "";
             gtbUsuario.ShadowDecoration.CustomizableEdges = customizableEdges4;
             gtbUsuario.Size = new Size(200, 32);
-            gtbUsuario.TabIndex = 35;
+            gtbUsuario.TabIndex = 49;
             // 
             // btnBuscar
             // 
@@ -182,11 +179,11 @@
             btnBuscar.FillColor2 = Color.FromArgb(11, 84, 144);
             btnBuscar.Font = new Font("Segoe UI", 9F);
             btnBuscar.ForeColor = Color.White;
-            btnBuscar.Location = new Point(32, 180);
+            btnBuscar.Location = new Point(150, 151);
             btnBuscar.Name = "btnBuscar";
             btnBuscar.ShadowDecoration.CustomizableEdges = customizableEdges6;
             btnBuscar.Size = new Size(63, 32);
-            btnBuscar.TabIndex = 34;
+            btnBuscar.TabIndex = 48;
             btnBuscar.Text = "Buscar";
             // 
             // Editar
@@ -203,11 +200,11 @@
             Editar.FillColor2 = Color.FromArgb(11, 84, 144);
             Editar.Font = new Font("Segoe UI", 9F);
             Editar.ForeColor = Color.White;
-            Editar.Location = new Point(377, 109);
+            Editar.Location = new Point(33, 203);
             Editar.Name = "Editar";
             Editar.ShadowDecoration.CustomizableEdges = customizableEdges8;
             Editar.Size = new Size(71, 27);
-            Editar.TabIndex = 33;
+            Editar.TabIndex = 47;
             Editar.Text = "Editar";
             // 
             // btnEliminar
@@ -224,11 +221,11 @@
             btnEliminar.FillColor2 = Color.FromArgb(11, 84, 144);
             btnEliminar.Font = new Font("Segoe UI", 9F);
             btnEliminar.ForeColor = Color.White;
-            btnEliminar.Location = new Point(470, 109);
+            btnEliminar.Location = new Point(29, 291);
             btnEliminar.Name = "btnEliminar";
             btnEliminar.ShadowDecoration.CustomizableEdges = customizableEdges10;
             btnEliminar.Size = new Size(71, 27);
-            btnEliminar.TabIndex = 32;
+            btnEliminar.TabIndex = 46;
             btnEliminar.Text = "Eliminar";
             // 
             // btnAgregar
@@ -245,51 +242,28 @@
             btnAgregar.FillColor2 = Color.FromArgb(11, 84, 144);
             btnAgregar.Font = new Font("Segoe UI", 9F);
             btnAgregar.ForeColor = Color.White;
-            btnAgregar.Location = new Point(280, 109);
+            btnAgregar.Location = new Point(31, 117);
             btnAgregar.Name = "btnAgregar";
             btnAgregar.ShadowDecoration.CustomizableEdges = customizableEdges12;
             btnAgregar.Size = new Size(71, 27);
-            btnAgregar.TabIndex = 31;
+            btnAgregar.TabIndex = 45;
             btnAgregar.Text = "Agregar";
             // 
-            // dataGridView2
+            // pictureBox4
             // 
-            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView2.Location = new Point(422, 227);
-            dataGridView2.Name = "dataGridView2";
-            dataGridView2.Size = new Size(415, 181);
-            dataGridView2.TabIndex = 43;
-            // 
-            // guna2TextBox1
-            // 
-            guna2TextBox1.AutoValidate = AutoValidate.EnablePreventFocusChange;
-            guna2TextBox1.BackColor = Color.Transparent;
-            guna2TextBox1.BorderColor = Color.Black;
-            guna2TextBox1.BorderRadius = 15;
-            guna2TextBox1.BorderStyle = System.Drawing.Drawing2D.DashStyle.Dot;
-            guna2TextBox1.BorderThickness = 0;
-            guna2TextBox1.CustomizableEdges = customizableEdges13;
-            guna2TextBox1.DefaultText = "Buscar tipo de producto";
-            guna2TextBox1.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
-            guna2TextBox1.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
-            guna2TextBox1.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
-            guna2TextBox1.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
-            guna2TextBox1.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
-            guna2TextBox1.Font = new Font("Segoe UI", 9F);
-            guna2TextBox1.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
-            guna2TextBox1.Location = new Point(523, 180);
-            guna2TextBox1.Name = "guna2TextBox1";
-            guna2TextBox1.PlaceholderText = "";
-            guna2TextBox1.SelectedText = "";
-            guna2TextBox1.ShadowDecoration.CustomizableEdges = customizableEdges14;
-            guna2TextBox1.Size = new Size(200, 32);
-            guna2TextBox1.TabIndex = 42;
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(47, 334);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(40, 35);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.TabIndex = 55;
+            pictureBox4.TabStop = false;
             // 
             // guna2GradientButton1
             // 
             guna2GradientButton1.BorderRadius = 15;
             guna2GradientButton1.BorderThickness = 1;
-            guna2GradientButton1.CustomizableEdges = customizableEdges15;
+            guna2GradientButton1.CustomizableEdges = customizableEdges13;
             guna2GradientButton1.DisabledState.BorderColor = Color.DarkGray;
             guna2GradientButton1.DisabledState.CustomBorderColor = Color.DarkGray;
             guna2GradientButton1.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
@@ -299,21 +273,20 @@
             guna2GradientButton1.FillColor2 = Color.FromArgb(11, 84, 144);
             guna2GradientButton1.Font = new Font("Segoe UI", 9F);
             guna2GradientButton1.ForeColor = Color.White;
-            guna2GradientButton1.Location = new Point(422, 180);
+            guna2GradientButton1.Location = new Point(29, 375);
             guna2GradientButton1.Name = "guna2GradientButton1";
-            guna2GradientButton1.ShadowDecoration.CustomizableEdges = customizableEdges16;
-            guna2GradientButton1.Size = new Size(63, 32);
-            guna2GradientButton1.TabIndex = 41;
-            guna2GradientButton1.Text = "Buscar";
+            guna2GradientButton1.ShadowDecoration.CustomizableEdges = customizableEdges14;
+            guna2GradientButton1.Size = new Size(75, 27);
+            guna2GradientButton1.TabIndex = 54;
+            guna2GradientButton1.Text = "Bodegas";
             // 
-            // frmTiposPS
+            // frmProveedores
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.GradientInactiveCaption;
-            ClientSize = new Size(858, 442);
-            Controls.Add(dataGridView2);
-            Controls.Add(guna2TextBox1);
+            ClientSize = new Size(629, 441);
+            Controls.Add(pictureBox4);
             Controls.Add(guna2GradientButton1);
             Controls.Add(guna2CustomGradientPanel1);
             Controls.Add(pictureBox3);
@@ -326,9 +299,8 @@
             Controls.Add(btnEliminar);
             Controls.Add(btnAgregar);
             FormBorderStyle = FormBorderStyle.None;
-            Name = "frmTiposPS";
-            StartPosition = FormStartPosition.CenterScreen;
-            Text = "frmTiposPS";
+            Name = "frmProveedores";
+            Text = "frmProveedores";
             guna2CustomGradientPanel1.ResumeLayout(false);
             guna2CustomGradientPanel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)pbxCerrar).EndInit();
@@ -336,7 +308,7 @@
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
-            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
             ResumeLayout(false);
         }
 
@@ -354,8 +326,7 @@
         private Guna.UI2.WinForms.Guna2GradientButton Editar;
         private Guna.UI2.WinForms.Guna2GradientButton btnEliminar;
         private Guna.UI2.WinForms.Guna2GradientButton btnAgregar;
-        private DataGridView dataGridView2;
-        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox1;
+        private PictureBox pictureBox4;
         private Guna.UI2.WinForms.Guna2GradientButton guna2GradientButton1;
     }
 }
