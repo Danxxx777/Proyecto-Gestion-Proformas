079d62c55f13e653606c551007d306d57d1e00aa
Troyano2004 <erwinbueno05@gmail.com>
2025-02-27 23:43:57 -0500

    frfr

---
+++ Proformas/Formularios/frmClienteProforma.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace Proformas.Formularios
+{
+    partial class frmClienteProforma
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges13 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges14 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges15 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges16 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            txtCedula = new Guna.UI2.WinForms.Guna2TextBox();
+            txtTipoCliente = new Guna.UI2.WinForms.Guna2TextBox();
+            txtEstado = new Guna.UI2.WinForms.Guna2TextBox();
+            txtCorreo = new Guna.UI2.WinForms.Guna2TextBox();
+            txtCliente = new Guna.UI2.WinForms.Guna2TextBox();
+            guna2HtmlLabel8 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            guna2HtmlLabel6 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            guna2HtmlLabel5 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            guna2HtmlLabel4 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            btnAgregar = new Guna.UI2.WinForms.Guna2GradientButton();
+            btnCerrar = new Guna.UI2.WinForms.Guna2GradientButton();
+            btnSeleccionar = new Guna.UI2.WinForms.Guna2GradientButton();
+            SuspendLayout();
+            // 
+            // txtCedula
+            // 
+            txtCedula.CustomizableEdges = customizableEdges1;
+            txtCedula.DefaultText = "";
+            txtCedula.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtCedula.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtCedula.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtCedula.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtCedula.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtCedula.Font = new Font("Segoe UI", 9F);
+            txtCedula.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtCedula.Location = new Point(228, 46);
+            txtCedula.Name = "txtCedula";
+            txtCedula.PlaceholderText = "";
+            txtCedula.SelectedText = "";
+            txtCedula.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            txtCedula.Size = new Size(157, 36);
+            txtCedula.TabIndex = 29;
+            txtCedula.TextChanged += txtCedula_TextChanged;
+            // 
+            // txtTipoCliente
+            // 
+            txtTipoCliente.CustomizableEdges = customizableEdges3;
+            txtTipoCliente.DefaultText = "";
+            txtTipoCliente.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtTipoCliente.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtTipoCliente.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtTipoCliente.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtTipoCliente.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtTipoCliente.Font = new Font("Segoe UI", 9F);
+            txtTipoCliente.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtTipoCliente.Location = new Point(228, 195);
+            txtTipoCliente.Name = "txtTipoCliente";
+            txtTipoCliente.PlaceholderText = "";
+            txtTipoCliente.SelectedText = "";
+            txtTipoCliente.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            txtTipoCliente.Size = new Size(116, 36);
+            txtTipoCliente.TabIndex = 28;
+            // 
+            // txtEstado
+            // 
+            txtEstado.CustomizableEdges = customizableEdges5;
+            txtEstado.DefaultText = "";
+            txtEstado.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtEstado.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtEstado.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtEstado.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtEstado.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtEstado.Font = new Font("Segoe UI", 9F);
+            txtEstado.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtEstado.Location = new Point(228, 313);
+            txtEstado.Name = "txtEstado";
+            txtEstado.PlaceholderText = "";
+            txtEstado.SelectedText = "";
+            txtEstado.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            txtEstado.Size = new Size(200, 36);
+            txtEstado.TabIndex = 27;
+            // 
+            // txtCorreo
+            // 
+            txtCorreo.CustomizableEdges = customizableEdges7;
+            txtCorreo.DefaultText = "";
+            txtCorreo.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtCorreo.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtCorreo.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtCorreo.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtCorreo.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtCorreo.Font = new Font("Segoe UI", 9F);
+            txtCorreo.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtCorreo.Location = new Point(228, 254);
+            txtCorreo.Name = "txtCorreo";
+            txtCorreo.PlaceholderText = "";
+            txtCorreo.SelectedText = "";
+            txtCorreo.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            txtCorreo.Size = new Size(200, 36);
+            txtCorreo.TabIndex = 26;
+            // 
+            // txtCliente
+            // 
+            txtCliente.CustomizableEdges = customizableEdges9;
+            txtCliente.DefaultText = "";
+            txtCliente.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtCliente.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtCliente.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtCliente.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtCliente.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtCliente.Font = new Font("Segoe UI", 9F);
+            txtCliente.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtCliente.Location = new Point(228, 129);
+            txtCliente.Name = "txtCliente";
+            txtCliente.PlaceholderText = "";
+            txtCliente.SelectedText = "";
+            txtCliente.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            txtCliente.Size = new Size(116, 36);
+            txtCliente.TabIndex = 25;
+            // 
+            // guna2HtmlLabel8
+            // 
+            guna2HtmlLabel8.BackColor = Color.Transparent;
+            guna2HtmlLabel8.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            guna2HtmlLabel8.Location = new Point(111, 195);
+            guna2HtmlLabel8.Name = "guna2HtmlLabel8";
+            guna2HtmlLabel8.Size = new Size(88, 22);
+            guna2HtmlLabel8.TabIndex = 24;
+            guna2HtmlLabel8.Text = "Tipo cliente:";
+            // 
+            // guna2HtmlLabel6
+            // 
+            guna2HtmlLabel6.BackColor = Color.Transparent;
+            guna2HtmlLabel6.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            guna2HtmlLabel6.Location = new Point(145, 254);
+            guna2HtmlLabel6.Name = "guna2HtmlLabel6";
+            guna2HtmlLabel6.Size = new Size(54, 22);
+            guna2HtmlLabel6.TabIndex = 23;
+            guna2HtmlLabel6.Text = "Correo:";
+            // 
+            // guna2HtmlLabel5
+            // 
+            guna2HtmlLabel5.BackColor = Color.Transparent;
+            guna2HtmlLabel5.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            guna2HtmlLabel5.Location = new Point(149, 313);
+            guna2HtmlLabel5.Name = "guna2HtmlLabel5";
+            guna2HtmlLabel5.Size = new Size(50, 22);
+            guna2HtmlLabel5.TabIndex = 22;
+            guna2HtmlLabel5.Text = "Estado";
+            // 
+            // guna2HtmlLabel1
+            // 
+            guna2HtmlLabel1.BackColor = Color.Transparent;
+            guna2HtmlLabel1.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            guna2HtmlLabel1.Location = new Point(123, 46);
+            guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            guna2HtmlLabel1.Size = new Size(76, 22);
+            guna2HtmlLabel1.TabIndex = 20;
+            guna2HtmlLabel1.Text = "N° Cédula:";
+            // 
+            // guna2HtmlLabel4
+            // 
+            guna2HtmlLabel4.BackColor = Color.Transparent;
+            guna2HtmlLabel4.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            guna2HtmlLabel4.Location = new Point(61, 129);
+            guna2HtmlLabel4.Name = "guna2HtmlLabel4";
+            guna2HtmlLabel4.Size = new Size(138, 22);
+            guna2HtmlLabel4.TabIndex = 21;
+            guna2HtmlLabel4.Text = "Nombre de Cliente:";
+            // 
+            // btnAgregar
+            // 
+            btnAgregar.BorderRadius = 15;
+            btnAgregar.BorderThickness = 1;
+            btnAgregar.CustomizableEdges = customizableEdges11;
+            btnAgregar.DisabledState.BorderColor = Color.DarkGray;
+            btnAgregar.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnAgregar.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnAgregar.DisabledState.FillColor2 = Color.FromArgb(169, 169, 169);
+            btnAgregar.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnAgregar.FillColor = Color.FromArgb(68, 3, 121);
+            btnAgregar.FillColor2 = Color.FromArgb(11, 84, 144);
+            btnAgregar.Font = new Font("Segoe UI", 9F);
+            btnAgregar.ForeColor = Color.White;
+            btnAgregar.Location = new Point(375, 381);
+            btnAgregar.Name = "btnAgregar";
+            btnAgregar.ShadowDecoration.CustomizableEdges = customizableEdges12;
+            btnAgregar.Size = new Size(139, 45);
+            btnAgregar.TabIndex = 45;
+            btnAgregar.Text = "Agregar";
+            btnAgregar.Click += btnGuardar_Click;
+            // 
+            // btnCerrar
+            // 
+            btnCerrar.BorderRadius = 15;
+            btnCerrar.BorderThickness = 1;
+            btnCerrar.CustomizableEdges = customizableEdges13;
+            btnCerrar.DisabledState.BorderColor = Color.DarkGray;
+            btnCerrar.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnCerrar.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnCerrar.DisabledState.FillColor2 = Color.FromArgb(169, 169, 169);
+            btnCerrar.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnCerrar.FillColor = Color.FromArgb(68, 3, 121);
+            btnCerrar.FillColor2 = Color.FromArgb(11, 84, 144);
+            btnCerrar.Font = new Font("Segoe UI", 9F);
+            btnCerrar.ForeColor = Color.White;
+            btnCerrar.Location = new Point(567, 381);
+            btnCerrar.Name = "btnCerrar";
+            btnCerrar.ShadowDecoration.CustomizableEdges = customizableEdges14;
+            btnCerrar.Size = new Size(139, 45);
+            btnCerrar.TabIndex = 46;
+            btnCerrar.Text = "Agregar";
+            btnCerrar.Click += btnCerrar_Click_1;
+            // 
+            // btnSeleccionar
+            // 
+            btnSeleccionar.BorderRadius = 15;
+            btnSeleccionar.BorderThickness = 1;
+            btnSeleccionar.CustomizableEdges = customizableEdges15;
+            btnSeleccionar.DisabledState.BorderColor = Color.DarkGray;
+            btnSeleccionar.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnSeleccionar.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnSeleccionar.DisabledState.FillColor2 = Color.FromArgb(169, 169, 169);
+            btnSeleccionar.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnSeleccionar.FillColor = Color.FromArgb(68, 3, 121);
+            btnSeleccionar.FillColor2 = Color.FromArgb(11, 84, 144);
+            btnSeleccionar.Font = new Font("Segoe UI", 9F);
+            btnSeleccionar.ForeColor = Color.White;
+            btnSeleccionar.Location = new Point(111, 381);
+            btnSeleccionar.Name = "btnSeleccionar";
+            btnSeleccionar.ShadowDecoration.CustomizableEdges = customizableEdges16;
+            btnSeleccionar.Size = new Size(139, 45);
+            btnSeleccionar.TabIndex = 47;
+            btnSeleccionar.Text = "Seleccionar";
+            // 
+            // frmClienteProforma
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnSeleccionar);
+            Controls.Add(btnCerrar);
+            Controls.Add(btnAgregar);
+            Controls.Add(txtCedula);
+            Controls.Add(txtTipoCliente);
+            Controls.Add(txtEstado);
+            Controls.Add(txtCorreo);
+            Controls.Add(txtCliente);
+            Controls.Add(guna2HtmlLabel8);
+            Controls.Add(guna2HtmlLabel6);
+            Controls.Add(guna2HtmlLabel5);
+            Controls.Add(guna2HtmlLabel1);
+            Controls.Add(guna2HtmlLabel4);
+            Name = "frmClienteProforma";
+            Text = "frmClienteProforma";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2TextBox txtCedula;
+        private Guna.UI2.WinForms.Guna2TextBox txtTipoCliente;
+        private Guna.UI2.WinForms.Guna2TextBox txtEstado;
+        private Guna.UI2.WinForms.Guna2TextBox txtCorreo;
+        private Guna.UI2.WinForms.Guna2TextBox txtCliente;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel8;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel6;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel5;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel4;
+        private Guna.UI2.WinForms.Guna2GradientButton btnAgregar;
+        private Guna.UI2.WinForms.Guna2GradientButton btnCerrar;
+        private Guna.UI2.WinForms.Guna2GradientButton btnSeleccionar;
+    }
+}
---
+++ Proformas/Formularios/frmClienteProforma.cs
@@ -0,0 +1,151 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace Proformas.Formularios
+{
+    public partial class frmClienteProforma : Form
+    {
+        private string connectionString = "Data Source=Ryzen7\\SQLEXPRESS;Initial Catalog=BDProformas;Integrated Security=True;Encrypt=False;Trust Server Certificate=True";
+
+        public string ClienteNombre { get; private set; } // Propiedad para devolver el nombre del cliente a frmProformas
+
+        public frmClienteProforma()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnGuardar_Click(object sender, EventArgs e)
+        {
+            string cedula = txtCedula.Text.Trim();
+            string nombre = txtCliente.Text.Trim();
+            string correo = txtCorreo.Text.Trim();
+            string tipoCliente = txtTipoCliente.Text.Trim();
+            string estado = txtEstado.Text.Trim();
+
+            // Validación de campos obligatorios
+            if (string.IsNullOrWhiteSpace(cedula) || string.IsNullOrWhiteSpace(nombre))
+            {
+                MessageBox.Show("Cédula y Nombre son obligatorios.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                await conn.OpenAsync();
+
+                // Verificar si el cliente ya existe
+                string queryVerificar = "SELECT COUNT(*) FROM Clientes WHERE NumeroIdentificacion = @Cedula";
+                using (SqlCommand cmdVerificar = new SqlCommand(queryVerificar, conn))
+                {
+                    cmdVerificar.Parameters.AddWithValue("@Cedula", cedula);
+                    int count = Convert.ToInt32(await cmdVerificar.ExecuteScalarAsync());
+
+                    if (count > 0)
+                    {
+                        MessageBox.Show("El cliente ya está registrado.", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        return;
+                    }
+                }
+
+                // Insertar el nuevo cliente
+                string queryInsertar = "INSERT INTO Clientes (NumeroIdentificacion, Nombre, Correo, TipoCliente, Estado, FechaRegistro) " +
+                                       "VALUES (@Cedula, @Nombre, @Correo, @TipoCliente, @Estado, GETDATE())";
+
+                using (SqlCommand cmdInsertar = new SqlCommand(queryInsertar, conn))
+                {
+                    cmdInsertar.Parameters.AddWithValue("@Cedula", cedula);
+                    cmdInsertar.Parameters.AddWithValue("@Nombre", nombre);
+                    cmdInsertar.Parameters.AddWithValue("@Correo", correo);
+                    cmdInsertar.Parameters.AddWithValue("@TipoCliente", tipoCliente);
+                    cmdInsertar.Parameters.AddWithValue("@Estado", estado);
+
+                    await cmdInsertar.ExecuteNonQueryAsync();
+                }
+            }
+
+            ClienteNombre = nombre; // Guardar el nombre del cliente para pasarlo a frmProformas
+            MessageBox.Show("Cliente registrado con éxito.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            this.DialogResult = DialogResult.OK; // Indica que se guardó correctamente
+            this.Close();
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtCedula_TextChanged(object sender, EventArgs e)
+        {
+            string texto = txtCedula.Text.Trim();
+            if (texto.Length == 10 && EsNumerico(texto))
+            {
+                BuscarClientePorCedula();
+            }
+        }
+        private async void BuscarClientePorCedula()
+        {
+            string cedula = txtCedula.Text.Trim(); // Usar el campo correcto para buscar por cédula
+            if (string.IsNullOrWhiteSpace(cedula) || cedula.Length != 10)
+            {
+                return; // No hace nada si el campo está vacío o no tiene 10 dígitos
+            }
+
+            string query = "SELECT Nombre, Correo, TipoCliente, Estado FROM Clientes WHERE NumeroIdentificacion = @cedula";
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    await conn.OpenAsync();
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@cedula", cedula);
+
+                        using (SqlDataReader reader = await cmd.ExecuteReaderAsync())
+                        {
+                            if (await reader.ReadAsync())
+                            {
+                                txtCliente.Invoke((MethodInvoker)(() => txtCliente.Text = reader["Nombre"].ToString()));
+                                txtCorreo.Invoke((MethodInvoker)(() => txtCorreo.Text = reader["Correo"].ToString()));
+                                txtTipoCliente.Invoke((MethodInvoker)(() => txtTipoCliente.Text = reader["TipoCliente"].ToString()));
+                                txtEstado.Invoke((MethodInvoker)(() => txtEstado.Text = reader["Estado"].ToString()));
+                            }
+                            else
+                            {
+                                LimpiarCampos();
+                                MessageBox.Show("No se encontraron clientes con esta cédula.", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al buscar cliente: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void LimpiarCampos()
+        {
+           
+            txtCorreo.Text = "";
+            txtCliente.Text = "";
+            txtEstado.Text = "";
+        }
+
+        private bool EsNumerico(string texto)
+        {
+            return int.TryParse(texto, out _);
+        }
+
+        private void btnCerrar_Click_1(object sender, EventArgs e)
+        {
+
+        }
+        
+    }
+}
+
